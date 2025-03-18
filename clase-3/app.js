7ea355e21c8754b301ee956ad8a54cbe8a8e3e27
Luis Monagas <monagasweb2020@gmail.com>
2025-03-18 00:45:39 -0400

    Creando una nueva película con POST

--- clase-3/app.js
+++ clase-3/app.js
@@ -1,34 +1,58 @@
 /** @format */
 
 const express = require('express');
-const path = require('path');
+const crypto = require('node:crypto');
+const path = require('node:path');
 const movies = require('./movies.json');
 
 const PORT = process.env.PORT ?? 1234;
 
 const app = express();
+app.use(express.json());
 app.disable('x-powered-by');
-
-app.get('/', (req, res) => {
-  res.json({ message: 'hola mundo' });
-});
 
 //Todos los recursos que sean MOVIES se identifica con /movies
 app.get('/movies', (req, res) => {
   const { genre } = req.query;
 
-  console.log(req.query);
+  if (genre) {
+    const filteredMovies = movies.filter((movie) =>
+      movie['genre'].some((g) => g.toLowerCase() == genre.toLowerCase())
+    );
+    return res.json(filteredMovies);
+  }
 
   res.json(movies);
 });
 
-app.get('/movies/:imdbID', (req, res) => {
+app.get('/movies/:id', (req, res) => {
   const { id } = req.params;
 
-  const movie = movies.find((movie) => movie.id == id);
+  const movie = movies.find((movie) => movie['id'] == id);
   if (movie) return res.json(movie);
 
   res.status(404).json({ message: 'Movie not found' });
+});
+
+app.post('/movies', (req, res) => {
+  const { title, genre, year, director, duration, rate, poster } = req.body;
+
+  const newMovie = {
+    id: crypto.randomUUID(),
+    title,
+    genre,
+    director,
+    year,
+    duration,
+    rate: rate ?? 0,
+    poster,
+  };
+  //Esto no seria REST, porque estamo guardando
+  //el estado de la aplicaciòn en memoria
+
+  movies.push(newMovie);
+
+  res.status(201).json(newMovie);
 });
 
 app.listen(PORT, () => {
