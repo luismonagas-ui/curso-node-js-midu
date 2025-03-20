d2038674e4ee860bdf2357ffebe138f76cfcb6b9
Luis Monagas <monagasweb2020@gmail.com>
2025-03-20 00:59:26 -0400

    Explicaciòn de CORS

--- clase-3/app.js
+++ clase-3/app.js
@@ -2,6 +2,7 @@
 
 const express = require('express');
 const crypto = require('node:crypto');
+const cors = require('cors');
 const path = require('node:path');
 const movies = require('./movies.json');
 const { validateMovie, validatePartialMovie } = require('./schemas.js');
@@ -11,10 +12,34 @@ const PORT = process.env.PORT ?? 1234;
 
 const app = express();
 app.use(express.json());
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      const ACCEPTED_ORIGINS = [
+        'http://localhost:8080',
+        'http://localhost:1234',
+        'https://movies.com',
+        'https://midu.dev',
+      ];
+
+      if (ACCEPTED_ORIGINS.includes(origin)) {
+        return callback(null, true);
+      }
+
+      if (!origin) {
+        return callback(null, true);
+      }
+
+      return callback(new Error('Not allowed by CORS'));
+    },
+  })
+);
 app.disable('x-powered-by');
 
 //Todos los recursos que sean MOVIES se identifica con /movies
 app.get('/movies', (req, res) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:8080');
+
   const { genre } = req.query;
 
   if (genre) {
@@ -54,6 +79,19 @@ app.post('/movies', (req, res) => {
   movies.push(newMovie);
 
   res.status(201).json(newMovie); //actualizar la caché del cliente
+});
+
+app.delete('/movies/:id', (req, res) => {
+  const { id } = req.params;
+  const movieIndex = movies.findIndex((movie) => movie.id === id);
+
+  if (movieIndex === -1) {
+    return res.status(404).json({ message: 'Movie not found' });
+  }
+
+  movies.splice(movieIndex, 1);
+
+  return res.json({ message: 'Movie deleted' });
 });
 
 app.patch('/movies/:id', (req, res) => {
