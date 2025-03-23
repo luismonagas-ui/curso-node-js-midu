e85ea03a6c55c99b1114e4e9cf4e4d2cf8952076
Luis Monagas <monagasweb2020@gmail.com>
2025-03-23 14:43:35 -0400

    Aplicando MVC mediante clases

--- clase-4/routes/movies-routes.js
+++ clase-4/routes/movies-routes.js
@@ -1,90 +1,16 @@
 /** @format */
 
 import { Router } from 'express';
-import { randomUUID } from 'node:crypto';
-import { readJSON } from '../utils/utils.js';
-import { validateMovie, validatePartialMovie } from '../schemas/movies.js';
+import { MovieController } from '../controllers/movies-controllers.js';
 
 export const moviesRouter = Router();
 
-const movies = readJSON('./data/movies.json');
+moviesRouter.get('/', MovieController.getAll);
 
-moviesRouter.get('/', (req, res) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:8080');
+moviesRouter.get('/:id', MovieController.getById);
 
-  const { genre } = req.query;
+moviesRouter.post('/', MovieController.create);
 
-  if (genre) {
-    const filteredMovies = movies.filter((movie) =>
-      movie['genre'].some((g) => g.toLowerCase() == genre.toLowerCase())
-    );
-    return res.json(filteredMovies);
-  }
+moviesRouter.delete('/:id', MovieController.delete);
 
-  res.json(movies);
-});
-
-moviesRouter.get('/:id', (req, res) => {
-  const { id } = req.params;
-
-  const movie = movies.find((movie) => movie['id'] == id);
-  if (movie) return res.json(movie);
-
-  res.status(404).json({ message: 'Movie not found' });
-});
-
-moviesRouter.post('/', (req, res) => {
-  const result = validateMovie(req.body);
-
-  if (result.error) {
-    //422 Unprocessable Entity
-    return res.status(422).json({ error: JSON.parse(result.error.message) });
-  }
-
-  const newMovie = {
-    id: randomUUID(),
-    ...result.data,
-  };
-  //Esto no seria REST, porque estamo guardando
-  //el estado de la aplicaciòn en memoria
-
-  movies.push(newMovie);
-
-  res.status(201).json(newMovie); //actualizar la caché del cliente
-});
-
-moviesRouter.delete('/:id', (req, res) => {
-  const { id } = req.params;
-  const movieIndex = movies.findIndex((movie) => movie.id === id);
-
-  if (movieIndex === -1) {
-    return res.status(404).json({ message: 'Movie not found' });
-  }
-
-  movies.splice(movieIndex, 1);
-
-  return res.json({ message: 'Movie deleted' });
-});
-
-moviesRouter.patch('/:id', (req, res) => {
-  const result = validatePartialMovie(req.body);
-
-  if (!result.success) {
-    return res.status(400).json({ error: JSON.parse(result.error.message) });
-  }
-
-  const { id } = req.params;
-  const movieIndex = movies.findIndex((movie) => movie['id'] == id);
-
-  if (movieIndex == -1)
-    return res.status(404).json({ message: 'Movie not found' });
-
-  const updateMovie = {
-    ...movies[movieIndex],
-    ...result.data,
-  };
-
-  movies[movieIndex] = updateMovie;
-
-  return res.json(updateMovie);
-});
+moviesRouter.patch('/:id', MovieController.update);
